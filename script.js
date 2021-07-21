1240cff672fff548d42e05f5f94a436ab8604102
AndreiDovan <dovan.andrei@yahoo.com>
2021-07-21 14:04:01 +0300

    First version of the website

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.getElementById("copy").innerHTML += new Date().getFullYear();
+
+$(document).ready(function(){
+    $('#name-wrap').fadeIn(2000);
+});
+
+$(document).ready(function(){
+    $('.logo').fadeIn(2000);
+});
+/*
+if($(window).width() >= 768)
+{
+    $(document).ready(function(){
+        $('#secondary-img').fadeIn(2000);
+    });
+    
+}*/
