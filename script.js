e189865c2b8ec06f8632605d54b69abb45e4616a
AndreiDovan <dovan.andrei@yahoo.com>
2021-07-23 15:58:50 +0300

    Implemented arrow animation and changed "education" section layout

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-
+// Date injection
 document.getElementById("copy").innerHTML += new Date().getFullYear();
 
+
+// Word ny word animation for intro
 $(document).ready(function(){
-    $('#name-wrap').fadeIn(2000);
+    $("#hi").fadeIn(1000);
+    $("#iam").delay(500).fadeIn(1000);
+    $('#name').delay(1000).fadeIn(2000);
 });
 
+
+// Animation for logo
 $(document).ready(function(){
     $('.logo').fadeIn(2000);
 });
+
+
+// Functionality for scroll arrow
+$(document).ready(function(){
+    $('.sageata').click(function(){
+        var y = $('.sageata').scrollTop();   
+        var window_height = window.innerHeight - 46;
+        $('html, body').animate({ scrollTop: y + window_height }) 
+    }); 
+ });
+
+
+
+ /*Function for highlighting arrow*/
+
+ $(function(){
+    function make_yellow(){
+       $(".sageata").css({"border-color": "black"});
+    }
+    function make_white(){
+        $(".sageata").css({"border-color": "rgb(216,216,216)"});
+    }
+    window.setTimeout( make_yellow, 2000 ); // 3 seconds
+    window.setTimeout( make_white, 2500);
+ });
+
+
+
+
 /*
 if($(window).width() >= 768)
 {
@@ -16,3 +51,4 @@ if($(window).width() >= 768)
     });
     
 }*/
+
